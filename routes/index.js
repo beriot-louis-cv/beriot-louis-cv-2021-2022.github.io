f6e7dd264d8064e14dd345a0ee63232c03d04112
LouisBerBiq <louis.beriotbiquet@gmail.com>
2021-12-23 17:36:14 +0100

    Added translation file reading

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,23 @@ var fs = require('fs');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  // TODO: check if exists and default to browser's language
   var translationFile = path.join(req.app.get('translation path'), req.query.language + '.json');
-  // TODO: make async
-  var translation = JSON.parse(fs.readFileSync(translationFile, 'utf8'));
+  var translation = ()=>{
+    if (fs.existsSync(translationFile)) {
+      return JSON.parse(fs.readFileSync(translationFile, 'utf8'));
+    } else {
+      //TODO: make a list of languages from available files
+      translationFile = path.join(req.app.get('translation path'), req.acceptsLanguages('fr', 'nl', 'en') + '.json');
+      if (fs.existsSync(translationFile)) {
+        return JSON.parse(fs.readFileSync(translationFile, 'utf8'));
+      } else {
+        translationFile = path.join(req.app.get('translation path'), 'en' + '.json');
+      }
+    };
+  }
+  
 
-  res.render('index', translation, function (err, html) {
+  res.render('index', translation(), function (err, html) {
     res.send(html);
   })
 });
