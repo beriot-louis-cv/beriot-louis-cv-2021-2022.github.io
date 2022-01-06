10262e54324a9a70709ff72316a63c1c52347117
LouisBerBiq <louis.beriotbiquet@gmail.com>
2022-01-06 05:37:48 +0100

    Made the FAQ P unrollable (still WIP)

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,25 @@
     const app = {
         init() {
             this.dropdown = document.querySelector('#language-dropdown');
-			this.submitDropDown();
+            this.faqItem = document.querySelectorAll('#faq li');
+			this.unrollFaqItem();
         },
         submitDropDown() {
             this.dropdown.addEventListener('change', (e) => {
 				e.target.form.submit();
 			});
+        },
+        unrollFaqItem() {
+            this.faqItem.forEach(el => {
+                el.addEventListener('mousedown', (e) => {
+                    if (e.target.tagName !== 'P') {
+                        let targetP = e.target.children[0];
+                        targetP.classList.toggle('unrolled');
+                    } else {
+                        return
+                    }
+                })
+            })
         }
     }
     app.init();
